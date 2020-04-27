7054fcfbf1b2e4844c9fb25ed4fda2f3e361e07d
felixgolcher <felix.golcher@hu-berlin.de>
2020-04-27 17:22:00 +0200

    alternative

--- krstn.R
+++ krstn.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 datasourceConfirmed <- "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 datasourceDeaths <- "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
 
@@ -30,7 +32,7 @@ confirmedDoubleLogRatio <- log(log(confirmed / confirmedDiff))
 deathsDoubleLogRatio <- log(log(deaths / deathsDiff))
 confirmedDoubleLogRatio[!is.finite(confirmedDoubleLogRatio)] <- NA
 deathsDoubleLogRatio[!is.finite(deathsDoubleLogRatio)] <- NA
-now
+
 
 dataRegion <- data.frame(days = 1:(dim(dataConfirmed)[2]-4),
                          date = as.Date(colnames(dataConfirmed)[-(1:4)], "X%m.%d.%y"),
@@ -44,14 +46,29 @@ dataRegion <- data.frame(days = 1:(dim(dataConfirmed)[2]-4),
 modelConfirmed <- lm(confirmedRatio ~ days, dataRegion, na.action = na.omit)
 modelDeaths <- lm(deathsRatio ~ days, dataRegion, na.action = na.omit)
 
+dataRegion
+
 library(ggplot2)
 library(gridExtra)
-DiffPlot <- ggplot(dataRegion, aes(date, confirmedRatio)) + 
-  ggtitle(paste("A: log(log(N/deltaN)) for", dataConfirmed[region, 2])) +
-  geom_point() +
-  geom_point(data = dataRegion, aes(date, deathsRatio), colour = 'red') 
-GumbelPlot <- ggplot(dataRegion, aes(date, confirmedDiff)) +
+(DiffPlot <- ggplot(dataRegion, aes(date, confirmedRatio)) + 
+    ggtitle(paste("A: log(log(N/deltaN)) for", dataConfirmed[region, 2])) +
+    geom_point() +
+    geom_point(data = dataRegion, aes(date, deathsRatio), colour = 'red')+
+    geom_smooth(data = dataRegion,
+                method = "lm", 
+                aes(date, deathsRatio),
+                method.args = list(na.action = na.omit)))
+(GumbelPlot <- ggplot(dataRegion, aes(date, confirmedDiff)) +
   ggtitle(paste("B: deltaN for", dataConfirmed[region, 2])) +
   geom_point() +
-  geom_point(data = dataRegion, aes(date, deathsDiff), colour = 'red')
+  geom_point(data = dataRegion, aes(date, deathsDiff), colour = 'red'))
 grid.arrange(DiffPlot, GumbelPlot, ncol=2)
+
+dataRegion %>%
+  select(days, date, ends_with("Ratio")) %>% 
+  pivot_longer(confirmedRatio:deathsRatio) %>% 
+  ggplot(aes(date, value, col=name))+
+  geom_point()+
+  geom_smooth(method="lm",
+              data=function(dtx)subset(dtx, name == "confirmedRatio"))
+  
